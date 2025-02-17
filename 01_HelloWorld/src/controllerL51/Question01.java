2187bdd0f95ef7ac97413ab1c557875a2847ed46
ART0066438 <ART0066438@SEN577876.educa.sf>
2025-02-17 16:38:27 -0300

    Comando While, questão 01

---
+++ 01_HelloWorld/src/controllerL51/Question01.java
@@ -0,0 +1,18 @@
+package controllerL51;
+
+public class Question01 {
+	/*
+	 * Desenvolver um programa que exiba os números de 1 a 100. 
+	 */
+
+	public static void main(String[] args) {
+		int contador = 1;
+		
+		while (contador <= 100) {
+			System.out.println(contador);
+			contador = contador + 1;
+		}
+
+	}
+
+}
