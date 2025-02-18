3aaa1ed5f78650a6ed2b4ff7656100a6a9859252
ART0066438 <ART0066438@SEN577876.educa.sf>
2025-02-18 16:30:22 -0300

    tem erro

---
+++ 01_HelloWorld/src/controllerL32/Question07.java
@@ -0,0 +1,16 @@
+package controllerL32;
+
+public class Question07 {
+	/*
+	 * A Loja Mamão com Açúcar está vendendo seus produtos em até 10 prestações sem juros.
+	 *  Faça um algoritmo que pergunte um valor de uma compra, 
+	 *  o número de prestações escolhidas 
+	 *  e apresente como resultado o valor das prestações. 
+	 */
+
+	public static void main(String[] args) {
+		double valorCompra, numPrest//continua
+
+	}
+
+}
